471e54b8af64ae779e068063c059f7bd07a32478
npolyak <nick.polyak@verizon.net>
2021-11-17 13:55:20 -0500

    checkpoint

--- Demos/NP.LocalizationPrototype/MainWindow.axaml.cs
+++ Demos/NP.LocalizationPrototype/MainWindow.axaml.cs
@@ -13,7 +13,8 @@ namespace NP.LocalizationPrototype
 {
     public partial class MainWindow : Window
     {
-        private ThemeLoader? _themeLoader;
+        private ThemeLoader? _languageThemeLoader;
+        private ThemeLoader? _colorThemeLoader;
 
         private Data _data = new Data("FirstDataTemplateText");
 
@@ -23,10 +24,14 @@ namespace NP.LocalizationPrototype
 #if DEBUG
             this.AttachDevTools();
 #endif
-            _themeLoader = 
+            _languageThemeLoader = 
                 Application.Current.Resources.GetThemeLoader("LanguageLoader");
 
-            this.GetObservable<Language>(SelectedLanguageProperty).Subscribe(OnSelectedLanguageChanged);
+            _colorThemeLoader =
+                Application.Current.Resources.GetThemeLoader("ColorLoader");
+
+            this.GetObservable(SelectedLanguageProperty).Subscribe(OnSelectedLanguageChanged);
+            this.GetObservable(SelectedColorThemeProperty).Subscribe(OnSelectedColorThemeChanged);
 
             ContentControl el1 = this.FindControl<ContentControl>("_elementI");
             ContentControl el2 = this.FindControl<ContentControl>("_elementII");
@@ -48,7 +53,12 @@ namespace NP.LocalizationPrototype
 
         private void OnSelectedLanguageChanged(Language language)
         {
-            _themeLoader!.SelectedDictionaryId = language.ToString();
+            _languageThemeLoader!.SelectedDictionaryId = language.ToString();
+        }
+
+        private void OnSelectedColorThemeChanged(ColorTheme colorTheme)
+        {
+            _colorThemeLoader!.SelectedDictionaryId = colorTheme.ToString();
         }
 
         private void ChangeUidButton_Click(object? sender, RoutedEventArgs e)
@@ -72,6 +82,20 @@ namespace NP.LocalizationPrototype
         #endregion SelectedLanguage Styled Avalonia Property
 
 
+        #region SelectedColorTheme Styled Avalonia Property
+        public ColorTheme SelectedColorTheme
+        {
+            get { return GetValue(SelectedColorThemeProperty); }
+            set { SetValue(SelectedColorThemeProperty, value); }
+        }
+
+        public static readonly StyledProperty<ColorTheme> SelectedColorThemeProperty =
+            AvaloniaProperty.Register<MainWindow, ColorTheme>
+            (
+                nameof(SelectedColorTheme)
+            );
+        #endregion SelectedColorTheme Styled Avalonia Property
+
 
         private void InitializeComponent()
         {
@@ -92,8 +116,8 @@ namespace NP.LocalizationPrototype
 
         private void ButtonError_Click(object sender, RoutedEventArgs e)
         {
-            string? message = _themeLoader?.GetResource<string>("NotEnoughMemory");
-            string? closeWindowStr = _themeLoader?.GetResource<string>("CloseWindowMessage"); ;
+            string? message = _languageThemeLoader?.GetResource<string>("NotEnoughMemory");
+            string? closeWindowStr = _languageThemeLoader?.GetResource<string>("CloseWindowMessage"); ;
 
             var window = CreateSampleWindow(message, closeWindowStr);
 
