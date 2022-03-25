8ceb34a417cd444e731934dc5293758794baafde
npolyak <nick.polyak@verizon.net>
2022-03-24 21:57:24 -0400

    created a demo project

---
+++ Demos/NP.DataGridFilteringDemo/MainWindow.axaml.cs
@@ -0,0 +1,26 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NP.DataGridFilteringDemo
+{
+    public partial class MainWindow : Window
+    {
+        public People ThePeople { get; } = new People();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+    }
+}
