36998413b207736a961bab23d16419c5c12aaf29
npolyak <nick.polyak@verizon.net>
2022-12-22 23:14:34 -0500

    added windows only functionality

---
+++ src/WindowsOnly/ImplantWindowUtils.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Controls;
+using Avalonia.Styling;
+using System;
+using System.Runtime.InteropServices;
+
+namespace NP.Avalonia.Visuals.WindowsOnly
+{
+    public static class ImplantWindowUtils
+    {
+        public static void ImplantWindow(Window parentWindow, IntPtr windowToImplantHandle)
+        {
+            WindowStyles windowToImplantStyle =
+                (WindowStyles) WinApi.GetWindowLongPtr(windowToImplantHandle, (int) WindowLongFlags.GWL_STYLE);
+
+            windowToImplantStyle &= ~WindowStyles.WS_BORDER;
+            windowToImplantStyle &= ~WindowStyles.WS_HSCROLL;
+            windowToImplantStyle &= ~WindowStyles.WS_VSCROLL;
+            windowToImplantStyle &= ~WindowStyles.WS_SIZEFRAME;
+            windowToImplantStyle &= ~WindowStyles.WS_POPUP;
+            windowToImplantStyle &= ~WindowStyles.WS_TABSTOP;
+            windowToImplantStyle &= ~WindowStyles.WS_DISABLED;
+            windowToImplantStyle &= ~WindowStyles.WS_SYSMENU;
+            windowToImplantStyle &= ~WindowStyles.WS_MINIMIZEBOX;
+            windowToImplantStyle &= ~WindowStyles.WS_DLGFRAME;
+            windowToImplantStyle &= ~WindowStyles.WS_MAXIMIZEBOX;
+            windowToImplantStyle &= ~WindowStyles.WS_CAPTION;
+            windowToImplantStyle |= WindowStyles.WS_CHILD;
+
+            HandleRef handleRef =
+                new HandleRef(null, windowToImplantHandle);
+
+            // set the new style of the schild window
+            WinApi.SetWindowLongPtr(handleRef, WindowLongFlags.GWL_STYLE, windowToImplantStyle);
+
+            // set the parent of the ProcessWindowHandle to be the main window's handle
+            WinApi.SetParent(windowToImplantHandle, parentWindow.PlatformImpl.Handle.Handle);
+        }
+    }
+}
