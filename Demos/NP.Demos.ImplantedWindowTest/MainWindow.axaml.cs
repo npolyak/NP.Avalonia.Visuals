dba948d588b00070e35a68a3e24b6e2c798642b1
npolyak <nick.polyak@verizon.net>
2022-12-22 23:22:26 -0500

    initial proj

---
+++ Demos/NP.Demos.ImplantedWindowTest/MainWindow.axaml.cs
@@ -0,0 +1,42 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using NP.Avalonia.Visuals.Controls;
+using System;
+
+namespace NP.Demos.BehaviorPrototypes
+{
+    public partial class MainWindow : CustomWindow
+    {
+
+        #region WindowPosition Styled Avalonia Property
+        public PixelPoint WindowPosition
+        {
+            get { return GetValue(WindowPositionProperty); }
+            set { SetValue(WindowPositionProperty, value); }
+        }
+
+        public static readonly StyledProperty<PixelPoint> WindowPositionProperty =
+            AvaloniaProperty.Register<MainWindow, PixelPoint>
+            (
+                nameof(WindowPosition)
+            );
+        #endregion WindowPosition Styled Avalonia Property
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
