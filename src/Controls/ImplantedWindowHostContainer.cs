c8dc45edb511f76a5dbd141d10c3993fccda70c5
npolyak <nick.polyak@verizon.net>
2023-02-22 00:27:23 -0500

    made the window processes dockable

--- src/Controls/ImplantedWindowHostContainer.cs
+++ src/Controls/ImplantedWindowHostContainer.cs
@@ -1,15 +1,9 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Platform;
-using Avalonia.Platform;
 using NP.Avalonia.Visuals.Behaviors;
 using System;
-using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 
-namespace NP.Avalonia.Visuals.WindowsOnly
+namespace NP.Avalonia.Visuals.Controls
 {
     public class ImplantedWindowHostContainer : Decorator
     {
@@ -98,58 +92,6 @@ namespace NP.Avalonia.Visuals.WindowsOnly
             ParentWindow = null;
 
             base.OnDetachedFromVisualTree(e);
-        }
-
-        class ImplantedWindowHost : NativeControlHost
-        {
-            private IntPtr _implantedWindowHandle;
-
-            public ImplantedWindowHost(IntPtr implantedWindowHandle)
-            {
-                _implantedWindowHandle = implantedWindowHandle;
-            }
-
-            protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
-            {
-                var parentWindow = (Window)e.Root;
-
-                parentWindow.ImplantWindow(_implantedWindowHandle);
-
-
-                // force refreshing the handle
-                MethodInfo? methodInfo =
-                    typeof(NativeControlHost)
-                        .GetMethod("DestroyNativeControl", BindingFlags.Instance | BindingFlags.NonPublic);
-
-                methodInfo?.Invoke(this, null);
-
-                base.OnAttachedToVisualTree(e);
-            }
-
-            protected override async void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
-            {
-                base.OnDetachedFromVisualTree(e);
-            }
-
-            protected override IPlatformHandle CreateNativeControlCore(IPlatformHandle parent)
-            {
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                {
-                    return new PlatformHandle(_implantedWindowHandle, "CTRL");
-                }
-                else
-                {
-                    return base.CreateNativeControlCore(parent);
-                }
-            }
-
-            protected override void DestroyNativeControlCore(IPlatformHandle control)
-            {
-                if (control is INativeControlHostDestroyableControlHandle)
-                {
-                    base.DestroyNativeControlCore(control);
-                }
-            }
         }
     }
 }
